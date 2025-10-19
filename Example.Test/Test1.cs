7f6231df46ab12a150de9fde1757f084ee5d46d7
Stefan Reichel <stefan@reichelhq.de>
2025-10-19 23:02:51 +0200

    init

---
+++ Example.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(false);
+    }
+}
